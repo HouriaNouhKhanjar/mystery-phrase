4be7f0846d1def2f903916c2f10daff8e400402f
HouriaNouhKhanjar <gls97048@students.codeinstitute.net>
2025-02-04 10:52:39 +0100

    generate random phrase

--- run.py
+++ run.py
@@ -1,2 +1,25 @@
+from wonderwords import RandomSentence
 
-print("hello")
+
+class MysteryPhrase:
+    """
+    Create a random phrase and define the properties of the words it contains,
+    then establish a set of procedures to handle them."
+    """
+    def __init__(self):
+        s = RandomSentence()
+        # Get a random sentence with a subject, predicate, direct object and adjective
+        self.phrase = s.sentence()
+
+
+
+def main():
+    """
+    Create phrase and call the procedures so that the game can begin.
+    """
+    print("Welcome to phrase chalnge")
+    mystery_phrase = MysteryPhrase()
+    print(mystery_phrase.phrase)
+
+
+main()
