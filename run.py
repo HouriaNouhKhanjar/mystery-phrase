fdc5a494034bcc065b14a50c596e0634ae013715
HouriaNouhKhanjar <gls97048@students.codeinstitute.net>
2025-02-04 11:31:16 +0100

    get words from phrase

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
 from wonderwords import RandomSentence
+import re
 
 
-class MysteryPhrase:
+class TextProcessor:
+    """
+    Creating Assistant Procedures for Analyzing Phrases and Words
+    """
+    def get_words(self):
+        """
+        Return the set of words contained in the sentence without punctuation marks.
+        """
+        phrase_without_punctuation = re.sub(r'[^\w\s]', '', self.phrase)
+        return phrase_without_punctuation.split()
+
+
+
+class MysteryPhrase(TextProcessor):
     """
     Create a random phrase and define the properties of the words it contains,
     then establish a set of procedures to handle them."
@@ -10,6 +24,9 @@ class MysteryPhrase:
         s = RandomSentence()
         # Get a random sentence with a subject, predicate, direct object and adjective
         self.phrase = s.sentence()
+    
+    def descripe(self):
+        print(f"   The generated phrase contains {len(self.get_words())} words.")
 
 
 
@@ -17,9 +34,16 @@ def main():
     """
     Create phrase and call the procedures so that the game can begin.
     """
-    print("Welcome to phrase chalnge")
+    print("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
+    print("   Welcome to Mystery Phrase ^_^ ")
+    print("   We will see your ability to discover the hidden words!\n")
     mystery_phrase = MysteryPhrase()
+    print("   The phrase has been generated for you.")
+    mystery_phrase.descripe()
+    print("   Let's go..... \n")
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
     print(mystery_phrase.phrase)
+    print(mystery_phrase.get_words())
 
 
 main()
