17e83250d0a9ce166a5b5c21a6531e74bb7ee5fe
HouriaNouhKhanjar <gls97048@students.codeinstitute.net>
2025-02-07 12:28:04 +0100

    reformat the code and add the libraries to requirements.txt

--- run.py
+++ run.py
@@ -2,30 +2,30 @@ from nltk.corpus import wordnet
 import nltk
 import spacy
 import random
-import re
 
 
 print("Loading data....")
 # Load spaCy model
 nlp = spacy.load('en_core_web_lg')
 # List of English words (could be expanded or replaced)
-word_list = [
-    "quick", "fox", "jumps", "over", "lazy", "dog", "cat", "happy", 
-    "silent", "run", "beautiful", "fast", "slow","sits", "under", 
-    "table", "sky", "clouds", "mountain", "river", "walks", "city", "night",
-    "apple", "book", "running", "game", "plays", "reading", "set"
-]
+word_list = ["quick", "fox", "jumps", "over", "lazy", "dog", "cat", "happy",
+             "silent", "run", "beautiful", "fast", "slow", "sits", "under",
+             "table", "sky", "clouds", "mountain", "river", "walks", "city",
+             "night", "apple", "book", "running", "game", "plays", "reading",
+             "set"]
+
 
 class TextProcessor:
     """
-    Creating Assistant Procedures for Generating Random Phrases, Analyzing Phrases and Words
+    Creating Assistant Procedures for Generating Random Phrases,
+    Analyzing Phrases and Words
     """
     def generate_random_phrase(self, length):
         """
         Generate a random phrase with a specific number of words
         """
         random.shuffle(word_list)
-        if length <= len (word_list):
+        if length <= len(word_list):
             return " ".join(word_list[:length])
         else:
             return " ".join(word_list)
@@ -35,7 +35,7 @@ class TextProcessor:
         Return the set of words contained in the sentence.
         """
         return phrase.split()
-    
+
     def get_pos_tag(self, phrase):
         """
         Return the part of speach tag of the phrase' words
@@ -46,16 +46,17 @@ class TextProcessor:
         return pos_tags
 
     def get_word_meaning(self, word):
-        """ 
+        """
         Get the meaning of a word
         """
-        synsets = wordnet.synsets(word)  # Get all the synsets (different meanings)
-        
+        # Get all the synsets (different meanings)
+        synsets = wordnet.synsets(word)
         if synsets:
             # Get the first meaning from the synsets
             return synsets[0].definition()
         else:
             return "Meaning not found."
+
 
 class MysteryWord(TextProcessor):
     """
@@ -69,51 +70,63 @@ class MysteryWord(TextProcessor):
         self.meaning = self.get_word_meaning(word)
         self.is_guessed = False
         self.available_attempts = 3
-        
+
     def describe(self):
-        print(f"This word is {self.length} carachter(s) long, starts with {self.start_with}")
+        print(f"This word is {self.length} carachter(s) long.")
+        print(f"starts with {self.start_with}.")
         print(f"POS_TAG for this word is: {self.pos_tag}")
         print(f"Words meaning: {self.meaning}")
-    
-    def validate(self,word):
-        number_attempts = f"{self.available_attempts-1} more attempts" if self.available_attempts>1 else "no more attempt"
+
+    def validate(self, word):
+        number_attempts = f"{self.available_attempts-1} more attempts" \
+                            if self.available_attempts > 1 \
+                            else "no more attempt"
         self.available_attempts -= 1
         if not word.isalpha():
-            return f"\nThe word contains other characters not only letters. You have {number_attempts}.\n"
+            return "\nThe word contains other characters not only letters." \
+                   f"\nYou have {number_attempts}.\n"
         elif self.word.lower() != word.lower():
-            return f"\nThe word was not guessed. The entered word is incorrect. You have {number_attempts}.\n"
-        
+            return "\nThe word was not guessed. The entered word is" \
+                   f"\nincorrect.You have {number_attempts}.\n"
+
         self.is_guessed = True
         return f"\nCongratulations!! you have guessed the word.\n"
-    
+
     def print_result(self):
-        print(f'{self.word:{15}} \t {self.is_guessed:{6}} \t {3-self.available_attempts:{6}}')
-    
+        print(f'{self.word:{15}} \t {self.is_guessed:{6}} \t \
+        {3-self.available_attempts:{6}}')
+
+
 class MysteryPhrase(TextProcessor):
     """
     Create a random phrase and define the properties of the words it contains,
     then establish a set of procedures to handle them."
     """
     def __init__(self):
-        # Get a random sentence with a subject, predicate, direct object and adjective
+        """
+        Get a random sentence with a subject, predicate,
+        direct object and adjective
+        """
         self.phrase = self.generate_random_phrase(4)
         self.pos_tags = []
         self.words = []
-    
+
     def define_words(self):
-        #get all words from the phrase without ponctuations
+        # get all words from the phrase without ponctuations
         word_list = self.get_words(self.phrase)
-        #get all pos tags for each words
+        # get all pos tags for each words
         self.pos_tags = self.get_pos_tag(self.phrase)
-        #generate defined word object list
-        self.words = [MysteryWord(word, self.pos_tags[word]) for word in word_list]
+        # generate defined word object list
+        self.words = [MysteryWord(word, self.pos_tags[word])
+                      for word in word_list]
 
     def describe(self):
         print(f"The generated phrase contains {len(self.words)} words.\n")
-    
+
     def display_current_state(self):
-        displayed_phrase = [word.word if word.is_guessed else "?" for word in self.words]
-        print (f"The current state of the phras: {displayed_phrase}\n")
+        displayed_phrase = [word.word if word.is_guessed
+                            else "?" for word in self.words]
+        print(f"The current state of the phrase: {displayed_phrase}\n")
 
 
 class Game(MysteryPhrase):
@@ -134,38 +147,47 @@ class Game(MysteryPhrase):
         self.describe()
         print("Let's go..... \n")
         print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
-        
 
     def play(self):
         self.display_current_state()
         print(f"{self.phrase}\n")
         for word in self.words:
-            print("-------------------------------------------------------------\n")
+            print("-------------------------------------------------"
+                  "------------\n")
             while word.available_attempts and not word.is_guessed:
                 word.describe()
                 user_word = input("Please enter a word: ")
                 validation = word.validate(user_word)
                 print(validation)
-            print("-------------------------------------------------------------\n")
+            print("-------------------------------------------------"
+                  "------------\n")
             self.display_current_state()
-    
+
     def get_final_result(self):
         print("Summery for your guessing scroe")
-        print(f'{"word":{15}} \t {"guessed":>{6}} \t {"number of used attempts":{6}}')
-        print(f'---------------------------------------------------------------------------------')
+        print(f'{"word":{15}} \t {"guessed":>{6}} \t \
+        {"number of used attempts":{6}}')
+        print("---------------------------------------------------------------"
+              "------------------")
         for word in self.words:
             word.print_result()
-        print(f'---------------------------------------------------------------------------------\n')
+        print("---------------------------------------------------------------"
+              "------------------\n")
         print("The correct phrase is:")
         print(f"{self.phrase}\n")
+        guessed_word = [word for word in self.words if word.is_guessed]
+        if len(guessed_word) == len(self.words):
+            print("Congratulations!! you have guessed all the words.\n")
+
 
 def main():
     """
     Create a game object and start and play the game.
     """
-    game= Game()
+    game = Game()
     game.start_game()
     game.play()
     game.get_final_result()
+
 
 main()
